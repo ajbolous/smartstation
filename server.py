3db9aa064089fc04925806b9caf8d3403a4749c0
t-abbolo@microsoft.com <t-abbolo@microsoft.com>
2018-04-11 15:08:03 +0300

    First commit server

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+@app.route("/sayHello")
+def hello(req):
+    print(req)
+    return "Hello World!"
+
+@app.route("/getStations")
+def addNumbers():
+
+    json.loads()
+    stations = [
+        {
+            "id": 1,
+            "name": "sfafri",
+            "location": {
+                "lat": 12323,
+                "lng": 123213
+            }
+        },
+        {
+            "id": 2,
+            "name": "sfafri",
+            "location": {
+                "lat": 12323,
+                "lng": 123213
+            }
+        }
+    ]
+    
+    j = json.dumps(stations, indent=4)
+    return j
+
+
+app.run()
