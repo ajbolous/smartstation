dbe37812b4727848545d0d12a39c39d630f0a444
t-abbolo@microsoft.com <t-abbolo@microsoft.com>
2018-04-17 18:37:15 +0300

    base and server

--- Server/database_handler.py
+++ Server/database_handler.py
@@ -1,15 +1,16 @@
 import json
-from model.station import fromJson, Station
-
+from model.station import Station
+import pymongo
 
 class DatabaseHandler():
     def __init__(self):
+        self.client = pymongo.MongoClient("mongodb://smartstations:eJz5FxWFCbkF39YqOe3hbUUWe9YVwE3V9Btv1cVNuMtESDtsVt9CA5qJIxTESVWeeQhZlgrzLTtOrPMUK8Ijeg==@smartstations.documents.azure.com:10255/?ssl=true&replicaSet=globaldb")
         self.stations = self.getStations()
 
     def getStations(self):
         stationObjects = []
-        file = open("/Users/lilian/smartstation/Server/data/stations.json", mode="r")
-        stations = json.load(file)
+        stations = self.client.main.stations.find()
         for station in stations:
-            stationObjects.append(fromJson(station))
+            stationObjects.append(Station.fromJson(station))
         return stationObjects
+
---
+++ Server/model/base.py
@@ -0,0 +1,14 @@
+class SerializableBase():
+    def __init__(self):
+        pass
+
+    def toJson(self):
+        return self.__dict__
+
+    @classmethod
+    def fromJson(cls, jsonObj):
+        obj = cls()
+        jsonObj.__delitem__("_id")
+        obj.__dict__.update(jsonObj)
+        print(jsonObj)
+        return obj
