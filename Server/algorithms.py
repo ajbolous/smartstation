30fc5cebff4633c5f8793459b644ff250a4c2213
Lilian <lilian@Lilians-MacBook-Air.local>
2018-04-19 18:41:35 +0300

    starting the algorithm writing

--- Server/algorithms.py
+++ Server/algorithms.py
@@ -1,8 +1,15 @@
+import networkx as nx
+from .model.route import Route
+from .model.station import Station
+from typing import List
 
-calcPath( src, dst , stations, routes)
+def buildGraph(stations:List[Station] , routes: List[Route], source:Station, destination:Station) -> nx.Graph:
+    graph = nx.Graph()
+    for station in stations:
+        graph.add_node(station.id)
 
-    1. get all relevant routes
-    2. graph = from releavt routes construct graph (stations) edge(w : time/distance)
-    3. path = find djikstra (src, dst, graph)
-    4. return path
-
+    return graph
+        
+def calcPaths(stations:List[Station], routes:List[Route], source:Station, destination:Station):
+    graph = buildGraph(stations, routes, source, destination)
+    
